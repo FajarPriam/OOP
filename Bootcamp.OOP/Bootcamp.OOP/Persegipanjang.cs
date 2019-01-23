23816a116d6b842089def12a0a6388e49e482a21
Fajar Priam <fajarpriam10@gmail.com>
2019-01-23 13:04:21 +0700

    OOP 23-1-2019

---
+++ Bootcamp.OOP/Bootcamp.OOP/Persegipanjang.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp.OOP
+{
+    class Persegipanjang : Bangun2D
+    {
+        private double panjang;
+        private double tinggip;
+
+        public Persegipanjang(double p, double i)
+        {
+            this.panjang = p;
+            this.tinggip = i;
+        }
+
+        public override double HitungLuas()
+        {
+            return (panjang * tinggip);
+        }
+    }
+}
---
+++ Bootcamp.OOP/Bootcamp.OOP/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp.OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bangun2D panggil;
+            Console.WriteLine("Perhitungan Luas Lingkaran");
+            Console.WriteLine("============================");
+            Console.WriteLine("1. Lingkaran");
+            Console.WriteLine("2. Segitiga");
+            Console.WriteLine("3. Persegi");
+            Console.WriteLine("4. Persegi Panjang");
+            Console.WriteLine("============================");
+            Console.Write("Masukkan pilihan anda (1-4): ");
+            string pilihan = Console.ReadLine();
+            int i = Convert.ToInt16(pilihan);
+            switch (i)
+            {
+                case 1:
+                    Console.Write("Masukkan Nilai Jari-jari:");
+                    double jari = Convert.ToDouble(Console.ReadLine());
+                    panggil = new Lingkaran(jari);
+                    panggil.CetakLuas("Lingkaran;");
+                    Console.Read();
+                    break;
+                case 2:
+                    Console.WriteLine("Masukkan Nilai Alas:");
+                    double alas = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Masukkan Nilai Tinggi");
+                    double tinggis = Convert.ToDouble(Console.ReadLine());
+                    panggil = new Segitiga(alas, tinggis);
+                    panggil.CetakLuas("Segitiga;");
+                    Console.Read();
+                    break;
+                case 3:
+                    Console.Write("Masukkan Nilai Sisi:");
+                    double sisi = Convert.ToDouble(Console.ReadLine());
+                    panggil = new Persegi(sisi);
+                    panggil.CetakLuas("Persegi;");
+                    Console.Read();
+                    break;
+                case 4:
+                    Console.WriteLine("Masukkan Nilai Alas:");
+                    double panjang = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Masukkan Nilai Tinggi");
+                    double tinggip = Convert.ToDouble(Console.ReadLine());
+                    panggil = new Persegipanjang(panjang, tinggip);
+                    panggil.CetakLuas("Persegipanjang;");
+                    Console.Read();
+                    break;
+                default:
+                    Console.WriteLine("Pilihan tidak ada -GOOD BYE-");
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
